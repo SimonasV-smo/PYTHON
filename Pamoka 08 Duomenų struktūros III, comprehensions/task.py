5a529d45ae3d784645620b555a23a8b97830d9aa
SimonasV-smo <siemkins@gmail.com>
2025-01-23 21:59:38 +0200

    tasks

--- Pamoka 08 Duomenų struktūros III, comprehensions/task.py
+++ Pamoka 08 Duomenų struktūros III, comprehensions/task.py
@@ -21,25 +21,66 @@
 # message = [f'Kaina : {eu} EUR' for eu in price_list]
 # print(message)
 
-# 2 task
+# # 2 task
+#
+# num_list = [1, 2, 3, 4, 5]
+# kaupiklis = [[elem, elem ** 2, elem ** 3, elem % 2 == 0] for elem in num_list]
+# print(kaupiklis)
+#
+# print('---------')
+#
+# num_list1 = [5, 8, 12, 18, 25, 30, 35, 40]
+# kaupiklis1 = [elem for elem in num_list1 if elem > 20]
+# print(kaupiklis1)
+#
+# print('---------')
+#
+# kaupiklis2 = [elem for elem in num_list1 if elem % 5 == 0]
+# print(kaupiklis2)
+#
+# print('---------')
+#
+# kaupiklis3 = ['Lyginis' if elem % 2 == 0 else 'nelyginis' for elem in num_list1]
+# print(kaupiklis3)
+#
 
-num_list = [1, 2, 3, 4, 5]
-kaupiklis = [[elem, elem ** 2, elem ** 3, elem % 2 == 0] for elem in num_list]
-print(kaupiklis)
+# 3 task
 
-print('---------')
+raides = ['A', 'B', 'C']
+skaiciai = ['1', '2', '3']
+#
+# rez = [raid + skaic for raid in raides for skaic in skaiciai]
+# print(rez)
+#
+# print('----------')
+#
+# rez2 = []
+# for raid in raides:
+#     for skaic in skaiciai:
+#         index_sum = raides.index(raid) + int(skaic)
+#         print(f"Raidė: {raid}, Skaičius: {skaic}, Indeksas: {raides.index(raid)}, Suma: {index_sum}")
+#         if index_sum > 3:
+#             rez2.append(raid + str(skaic))
+#
+# print(rez2)
 
-num_list1 = [5, 8, 12, 18, 25, 30, 35, 40]
-kaupiklis1 = [elem for elem in num_list1 if elem > 20]
-print(kaupiklis1)
+# print('----------')
+#
+# rez3 = [raid.lower() + str(skaic) for raid in raides for skaic in skaiciai if int(skaic) % 2 == 0]
+# print(rez3)
 
-print('---------')
+print('----------')
 
-kaupiklis2 = [elem for elem in num_list1 if elem % 5 == 0]
-print(kaupiklis2)
+listas = [1, 2, 3, 2, 1, 4, 5, 5]
 
-print('---------')
+set_rez = {num for num in listas}
+print(set_rez)
 
-kaupiklis3 = ['Lyginis' if elem % 2 == 0 else 'nelyginis' for elem in num_list1]
-print(kaupiklis3)
+print('----------')
 
+tuple_rez = tuple(num + 1 for num in listas)
+print(tuple_rez)
+
+print('----------')
+
+dict_rez = {num: num}
