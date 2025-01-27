1f66dc2e9618d3fc07dd837370d0b634e170ef60
SimonasV-smo <siemkins@gmail.com>
2025-01-27 19:00:38 +0200

    tasks

--- Pamoka 09 Funkcijos I/task.py
+++ Pamoka 09 Funkcijos I/task.py
@@ -3,7 +3,7 @@
 
 user_name = input('Jusu vardas: ')
 def sveikink():
-    print(f'Labas, {user_name}. Smagu tave matyti!')
+    for i in range(5):
+        print(f'Labas, {user_name}. Smagu tave matyti!')
 
-for i in range(5):
-    sveikink()
+sveikink()
