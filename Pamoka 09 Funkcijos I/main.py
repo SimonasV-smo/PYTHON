ff372cd9ebcef664dc181f7fee0780aa6e5561df
SimonasV-smo <siemkins@gmail.com>
2025-01-27 18:23:15 +0200

    tasks

---
+++ Pamoka 09 Funkcijos I/main.py
@@ -0,0 +1,10 @@
+
+
+def say_hello():
+    print('Hello, user!')
+
+def say_bye():
+    ...
+
+for i in range(100):
+    say_hello()
---
+++ Pamoka 09 Funkcijos I/task.py
@@ -0,0 +1,9 @@
+
+# 1 task\\Ivadas i funkcija
+
+user_name = input('Jusu vardas: ')
+def sveikink():
+    print(f'Labas, {user_name}. Smagu tave matyti!')
+
+for i in range(5):
+    sveikink()
