9be9731fb9312eb62e25bd3a0a31c8e2df1b0ea2
SimonasV-smo <siemkins@gmail.com>
2025-01-27 19:38:21 +0200

    tasks

--- Pamoka 09 Funkcijos I/task.py
+++ Pamoka 09 Funkcijos I/task.py
@@ -1,9 +1,19 @@
 
-# 1 task\\Ivadas i funkcija
+# # 1 task\\Ivadas i funkcija
+#
+# user_name = input('Jusu vardas: ')
+# def sveikink():
+#     for i in range(5):
+#         print(f'Labas, {user_name}. Smagu tave matyti!')
+#
+# sveikink()
 
-user_name = input('Jusu vardas: ')
-def sveikink():
-    for i in range(5):
-        print(f'Labas, {user_name}. Smagu tave matyti!')
+# 2 task\\ Argumentai ir return reiksmes
 
-sveikink()
+x = int(input('Pirmas daugiklis: '))
+y = int(input('Atras daugiklis: '))
+
+def sudaugink(x, y):
+    rez = x * y
+    print('rezultatas = ', rez )
+sudaugink(x, y)
