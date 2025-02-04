5bd1227efdec27ea322c2d1122360436de33c59e
SimonasV-smo <siemkins@gmail.com>
2025-02-04 20:37:26 +0200

    task

--- Pamoka 14 Objektinis programavimas I ‐ klasės, instance objektai/main.py
+++ Pamoka 14 Objektinis programavimas I ‐ klasės, instance objektai/main.py
@@ -1,5 +1,42 @@
 
 
+# class House:
+#     country = 'LT'
+#
+#     def __init__(self, price, year):
+#         self.price = price
+#         self.year = year
+#
+# house1 = House(199000, 2025)
+# house2 = House(165000, 2008)
+#
+# house1.country = 'LV'
+#
+# print(
+#     house1.country, house1.price, house1.year
+# )
+# print(
+#     house2.country, house2.price, house2.year
+# )
+#
+# class House:
+#     country = 'LT'
+#
+#     def __init__(self, price, year):
+#         self.price = price
+#         self.year = year
+#
+#     def __str__(self):
+#         print('123')
+#
+# house_instance1 = House(10_000, 1990)
+# house_instance2 = House(12_000, 1920)
+#
+# print(house_instance1)
+# print(house_instance2)
+
+from datetime import datetime
+
 class House:
     country = 'LT'
 
@@ -7,14 +44,14 @@ class House:
         self.price = price
         self.year = year
 
-house1 = House(199000, 2025)
-house2 = House(165000, 2008)
+    def get_age(self):
+        current_year = datetime.today().year
+        return  current_year - self.year
 
-house1.country = 'LV'
+house_instance1 = House(10_000, 1990)
+age = house_instance1.get_age()
+print(age)
 
-print(
-    house1.country, house1.price, house1.year
-)
-print(
-    house2.country, house2.price, house2.year
-)
+house_instance2 = House(5_000_000, 2001)
+age = house_instance2.get_age()
+print(age)
--- Pamoka 14 Objektinis programavimas I ‐ klasės, instance objektai/task.py
+++ Pamoka 14 Objektinis programavimas I ‐ klasės, instance objektai/task.py
@@ -1,39 +1,61 @@
+#
+# # 1 task
+#
+# class Car:
+#     manufacturer = 'Audi'
+#
+# print(Car.manufacturer)
+#
+# class Bike:
+#     manufacturer = 'Ducati'
+#
+# print(Bike.manufacturer)
+#
+# print('-' * 25)
+#
+# # 2task
+#
+# class Book:
+#     def __init__(self, title, author, year):
+#         self.title = title
+#         self.author = author
+#         self.year = year
+#
+# book1 = Book('1984', 'George Orwell', 1949 )
+# book2 = Book('Everything Is F*cked', 'Mark Manson', 2019)
+#
+# print(
+#     f'Knyga: "{book1.title}", autorius: {book1.author}, prasyta: {book1.year} metais.'
+# )
+# print(
+#     f'Knyga: "{book2.title}", autorius: {book2.author}, prasyta: {book2.year} metais.'
+# )
+#
+# print('-' * 25)
+#
+# # 3task
+#
+# class Book:
+#     publisher = 'Penguin'
+#
+#     def __init__(self, title, author, year):
+#         self.title = title
+#         self.author = author
+#         self.year = year
+#
+# book1 = Book('1984', 'George Orwell', 1949 )
+# book2 = Book('Everything Is F*cked', 'Mark Manson', 2019)
+#
+# print(
+#     f'Knyga: "{book1.title}", autorius: {book1.author}, leidejas : {Book.publisher}.'
+# )
+# print(
+#     f'Knyga: "{book2.title}", autorius: {book2.author}, leidejas: {Book.publisher}.'
+# )
+#
+# 4 task
 
-# 1 task
 
-class Car:
-    manufacturer = 'Audi'
-
-print(Car.manufacturer)
-
-class Bike:
-    manufacturer = 'Ducati'
-
-print(Bike.manufacturer)
-
-print('-' * 25)
-
-# 2task
-
-class Book:
-    def __init__(self, title, author, year):
-        self.title = title
-        self.author = author
-        self.year = year
-
-book1 = Book('1984', 'George Orwell', 1949 )
-book2 = Book('Everything Is F*cked', 'Mark Manson', 2019)
-
-print(
-    f'Knyga: "{book1.title}", autorius: {book1.author}, prasyta: {book1.year} metais.'
-)
-print(
-    f'Knyga: "{book2.title}", autorius: {book2.author}, prasyta: {book2.year} metais.'
-)
-
-print('-' * 25)
-
-# 3task
 
 class Book:
     publisher = 'Penguin'
@@ -43,13 +65,62 @@ class Book:
         self.author = author
         self.year = year
 
-book1 = Book('1984', 'George Orwell', 1949 )
-book2 = Book('Everything Is F*cked', 'Mark Manson', 2019)
+book_instance1 = Book('1984', 'George Orwell', 1949 )
+book_instance2 = Book('Everything Is F*cked', 'Mark Manson', 2019)
 
-print(
-    f'Knyga: "{book1.title}", autorius: {book1.author}, leidejas : {Book.publisher}.'
-)
-print(
-    f'Knyga: "{book2.title}", autorius: {book2.author}, leidejas: {Book.publisher}.'
-)
+print(book_instance1)
+print(book_instance2)
 
+print('-' * 25)
+
+# 5task
+
+from datetime import datetime
+
+class Book:
+    publisher = 'Penguin'
+
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def get_book_age(self):
+        current_year = datetime.today().year
+        return current_year - self.year
+
+book_instance1 = Book('1984', 'George Orwell', 1949 )
+age = book_instance1.get_book_age()
+print(age)
+
+book_instance2 = Book('Everything Is F*cked', 'Mark Manson', 2019)
+age = book_instance2.get_book_age()
+print(age)
+
+print('-' * 25)
+
+# 6task
+
+from datetime import datetime
+
+class Book:
+    publisher = 'Penguin'
+
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def get_book_age(self):
+        current_year = datetime.today().year
+        return current_year - self.year
+
+book_instance1 = Book('1984', 'George Orwell', 1949 )
+age = book_instance1.get_book_age()
+print(f'Knyga <{book_instance1.title}> yra <{age}> metu senumo.')
+
+book_instance2 = Book('Everything Is F*cked', 'Mark Manson', 2019)
+age = book_instance2.get_book_age()
+print(f'Knyga <{book_instance2.title}> yra <{age}> metu senumo.')
+
+print('-' * 25)
