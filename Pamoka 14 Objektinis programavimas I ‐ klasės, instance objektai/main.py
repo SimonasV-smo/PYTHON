82b9039e9be1b56ef05c30a468f1a45d1f6bcde2
SimonasV-smo <siemkins@gmail.com>
2025-02-05 18:04:23 +0200

    task

--- Pamoka 14 Objektinis programavimas I ‐ klasės, instance objektai/main.py
+++ Pamoka 14 Objektinis programavimas I ‐ klasės, instance objektai/main.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+# from datetime import datetime
 
 
 # class House:
@@ -56,6 +56,41 @@ from datetime import datetime
 # house_instance2 = House(5_000_000, 2001)
 # age = house_instance2.get_age()
 # print(age)
+#
+# class House:
+#     country = 'LT'
+#
+#     def __init__(self, price, year):
+#         self.price = price
+#         self.year = year
+#
+#     def __str__(self):
+#         return f'Namas {self.year}, kaina - {self.price}, amzius {self.get_age()}'
+#
+#     def get_age(self):
+#         return datetime.today().year - self.year
+
+# book1 = House(10_000_000, 2024)
+# print(book1)
+
+
+# houses = [
+#     House(22_000, 1980),
+#     House(15_000, 1970),
+#     House(10_000, 1960)
+# ]
+
+# for house in houses:
+#     print(f'sena kaina: {house.price}')
+#     house.price = round(house.price * 1.21)
+#     print(f'nauja kaina: {house.price}')
+#     print('-' * 40)
+
+# for house in houses:
+#     print(house)
+
+from datetime import datetime
+
 
 class House:
     country = 'LT'
@@ -65,10 +100,19 @@ class House:
         self.year = year
 
     def __str__(self):
-        return f'Namas {self.year}, kaina - {self.price}, amzius {self.get_age()}'
+        return f'Namas {self.year} pastatymo, kaina - {self.price}, amžius {self.get_age()}'
 
     def get_age(self):
         return datetime.today().year - self.year
 
-book1 = House(10_000_000, 2024)
-print(book1)
+houses_kaupiklis = []
+while True:
+    year = int(input('Iveskite metus: '))
+    price = int(input('Iveskite kaina: '))
+
+    house_instance = House(price, year)
+    houses_kaupiklis.append(house_instance)
+
+    print('Spausdinam ka sukaupem:')
+    for house in houses_kaupiklis:
+        print(house)
