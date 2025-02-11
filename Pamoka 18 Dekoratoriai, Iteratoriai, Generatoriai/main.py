8062a448ac8dba44bcfbd9565ce641d284cf543c
SimonasV-smo <siemkins@gmail.com>
2025-02-11 20:51:05 +0200

    task

---
+++ Pamoka 18 Dekoratoriai, Iteratoriai, Generatoriai/main.py
@@ -0,0 +1,65 @@
+
+# Dekoratoriai
+
+class Darbuotojai:
+    def __init__(self, vardas, pavarde, pareigos):
+        self.vardas = vardas
+        self.pavarde = pavarde
+        self.pareigos = pareigos
+
+    def change_employee_name(self, vardas):
+        if not vardas:
+            raise ValueError('Name is not provided')
+        self.vardas = vardas
+
+    @staticmethod
+    def divide(a, b):
+        if b == 0:
+            raise ZeroDivisionError('van not divide by zero')
+        return a / b
+
+    @classmethod
+    def super_constructor(cls, vardas, pavarde, pareigos):
+        return cls(vardas, pavarde, pareigos)
+
+    @property
+    def atlyginimas(self):
+        return self.__atlyginimas
+
+def registratorius(funkcija):
+    def apvalkalas(argumentas):
+        rezultatas = funkcija(argumentas)
+        if rezultatas % 2 ==0:
+            print(f'{rezultatas} yra lyginis')
+        else:
+            print(f'{rezultatas} yra nelyginis')
+        return rezultatas
+    return apvalkalas
+
+@registratorius
+def kvadratu(skaicius):
+    return skaicius ** 2
+
+print(kvadratu(8))
+
+def top_level_delay(seconds):
+    def returner_of_delayed_func(func):
+        def wrapper(*args, **kwargs):
+            print(f"Funkcijos darbas prasides po: {seconds}")
+            time.sleep(seconds)
+            res = func(*args, **kwargs)
+            return res
+        return wrapper
+    return returner_of_delayed_func
+
+@top_level_delay(9)
+def grazink500():
+    return 500
+
+@top_level_delay(15)
+def vykdyk_aritmetika(skaicius1, skaicius2, veiksmas=None):
+    if veiksmas == "atimtis":
+        return skaicius1 - skaicius2
+
+res = grazink500()
+print(res)
--- Pamoka 18 Dekoratoriai, Iteratoriai, Generatoriai/task.py
+++ Pamoka 18 Dekoratoriai, Iteratoriai, Generatoriai/task.py
@@ -1,25 +1,57 @@
+#
+# # 1. Funkcijos kaip Pirmos Klasės Objektai
+#
+# def prideti_zenkliuka(tekstas):
+#     return f'{tekstas}*'
+#
+# def apversti_sakini(tekstas):
+#     return tekstas[::-1]
+#
+# print(apversti_sakini('sis tekstas yra apverstas'))
+# print(prideti_zenkliuka('siam tekstui reikia zenkliuko'))
+#
+# def apdoroti_teksta(tekstas, funkcija = None):
+#     if funkcija:
+#         tekstas = funkcija(tekstas)
+#     return tekstas.lower()
+#
+# def keli_apdorojimai(tekstas, *funkcijos):
+#     for funkcija in funkcijos:
+#         tekstas = funkcija(tekstas)
+#     return tekstas
+#
+# print(apdoroti_teksta('Sis Tekstas yra mažosiomis raidėmis'))
+# print(apdoroti_teksta('sis tekstas su funkcija', prideti_zenkliuka))
+# print(keli_apdorojimai('sis tekstas su keliomis funkcijomis', prideti_zenkliuka, apversti_sakini))
 
-# 1. Funkcijos kaip Pirmos Klasės Objektai
+# 2. Dekoratoriai
 
-def prideti_zenkliuka(tekstas):
-    return f'{tekstas}*'
+def sekimo_dekoratorius(funkcija):
+    def apvalkalas(*args, **kwargs):
+        print(f'Vykdoma funkcija : {funkcija.__name__}')
+        res = funkcija(*args, **kwargs)
+        print('Funkcija baigta.')
+        return res
+    return apvalkalas
 
-def apversti_sakini(tekstas):
-    return tekstas[::-1]
+@sekimo_dekoratorius
+def dauginti(a, b):
+    return a * b
 
-print(apversti_sakini('sis tekstas yra apverstas'))
-print(prideti_zenkliuka('siam tekstui reikia zenkliuko'))
+@sekimo_dekoratorius
+def dalinti(a, b):
+    if b == 0:
+        return 'dalyba is nulio negalima'
+    return a / b
 
-def apdoroti_teksta(tekstas, funkcija = None):
-    if funkcija:
-        tekstas = funkcija(tekstas)
-    return tekstas.lower()
+print("Dauginimo testas:")
+rezultatas1 = dauginti(5, 3)
+print(f"Rezultatas: {rezultatas1}\n")
 
-def keli_apdorojimai(tekstas, *funkcijos):
-    for funkcija in funkcijos:
-        tekstas = funkcija(tekstas)
-    return tekstas
+print("Dalybos testas:")
+rezultatas2 = dalinti(10, 2)
+print(f"Rezultatas: {rezultatas2}\n")
 
-print(apdoroti_teksta('Sis Tekstas yra mažosiomis raidėmis'))
-print(apdoroti_teksta('sis tekstas su funkcija', prideti_zenkliuka))
-print(keli_apdorojimai('sis tekstas su keliomis funkcijomis', prideti_zenkliuka, apversti_sakini))
+print("Dalybos iš nulio testas:")
+rezultatas3 = dalinti(8, 0)
+print(f"Rezultatas: {rezultatas3}")
