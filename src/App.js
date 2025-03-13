88ff1980fb2ee8dbe9da2693244420e61ce3c665
SimonasV-smo <siemkins@gmail.com>
2025-03-13 20:41:54 +0200

    task

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const vardas ='Simonas';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sveiki atvyke i React!</h1>
+      <h1>Sveiki, {vardas}!</h1>
     </div>
   );
 }
-
+ 
 export default App;
+ 
